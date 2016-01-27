936e89df1f5c057c8ed364e36811e1bac0d66d8c
MonkeyFirst <monkey3dcg@gmail.com>
2016-01-28 03:30:02 +0600

    gs-shaders-c2

--- bin/CoreData/Shaders/GLSL/Shadow.glsl
+++ bin/CoreData/Shaders/GLSL/Shadow.glsl
@@ -30,7 +30,16 @@ void PS()
 
     #ifdef VSM_SHADOW
         float depth = vTexCoord.z;
-        gl_FragColor = vec4(depth, depth * depth, 1.0, 1.0);
+        depth = depth * 0.5 + 0.5;
+		float moment1 = depth;
+		float moment2 = depth * depth;
+		
+        // Adjusting moments (this is sort of bias per pixel) using partial derivative
+		float dx = dFdx(depth);
+		float dy = dFdy(depth);
+		moment2 += 0.25*(dx*dx+dy*dy);
+        
+        gl_FragColor = vec4(moment1, moment2, 0.0, 0.0);
     #else
         gl_FragColor = vec4(1.0);
     #endif
